cfb853a47e3be5a5da6a2f223db187de06661cfa
ChoiSangIl <wo566@naver.com>
2022-01-10 20:11:04 +0900

    swagger 적용

--- msa-study-order/src/main/java/msa/study/order/controller/api/OrderController.java
+++ msa-study-order/src/main/java/msa/study/order/controller/api/OrderController.java
@@ -3,9 +3,12 @@ package msa.study.order.controller.api;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 @RestController
 public class OrderController {
 
+	@Operation(summary = "주문 api", description = "주문 api")
 	@GetMapping("order")
 	private String order() {
 		return "order...";
